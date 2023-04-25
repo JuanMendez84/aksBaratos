0a2edd3873381c2ccdb6da93ae790c1076e47bfc
Juan Mendez <omailovin@gmail.com>
2023-04-25 17:06:24 +0200

    Ahora cada fila es un objeto cuyos atributos son NOMBRE, ULR y PRECIO

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
+class Juego:
+    url=""
+    nombre=""
+    precio=0.0
+
+    def __init__(self, url, nombre, precio):
+        self.url=url
+        self.nombre=nombre
+        self.precio=precio
+
+    def __str__(self):
+        return "Nombre:{}\nURL:{}\nPrecio:{}\n\n".format(self.nombre, self.url, self.precio)
+
 url_base="https://www.allkeyshop.com/blog/list/Juan-Mendez2/336592/"
 
 req = requests.get(url_base)
@@ -8,10 +21,22 @@ soup = BeautifulSoup(req.text, features="html.parser")
 
 filas_wl = soup.select(".akswl-list-display tr a")
 
-lista_nombres=[]
+lista_juegos=[]
+
+url=""
+nombre=""
+precio=0.0
+
+print(lista_juegos)
 
 for i,fila in enumerate(filas_wl):
     if i == 1 or (i-1)%3 == 0:
-        lista_nombres.append(fila.getText().strip())
+        nombre=fila.getText().strip()
+        url=fila['href']
+    if i-1 == 1 or (i-2)%3 == 0:
+        precio=fila.getText().strip()[:-1]
+        lista_juegos.append(Juego(url,nombre,precio))
 
-print(lista_nombres)
+for juego in lista_juegos:
+    print(juego)
+print(len(lista_juegos))
