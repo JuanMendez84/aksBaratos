b886939e1f87f37179a410309ab0a0ac05c86ca8
Juan Mendez <omailovin@gmail.com>
2023-04-29 21:26:26 +0200

    he extraido la tabla con las tiendas del primer juego

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ class Juego:
 
     def __str__(self):
         return "Nombre:{}\nURL:{}\nPrecio:{}\n\n".format(self.nombre, self.url, self.precio)
+    
+    def obtiene_soup(self):
+        req = requests.get(self.url)
+        soup = BeautifulSoup(req.text, features="html.parser")
+
+        return soup
+
 
 url_base="https://www.allkeyshop.com/blog/list/Juan-Mendez2/336592/"
 
@@ -38,5 +45,9 @@ for i,fila in enumerate(filas_wl):
         lista_juegos.append(Juego(url,nombre,precio))
 
 for juego in lista_juegos:
-    print(juego)
-print(len(lista_juegos))
+    soup_juego = juego.obtiene_soup()
+    tabla_tiendas = soup.select("table")
+    print(tabla_tiendas)
+    break
+
+#hola
